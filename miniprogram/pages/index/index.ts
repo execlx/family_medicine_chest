afa44b26ef740187af4c24b62cb8467e8fdbac93
codermy <gsmxsky@gmail.com>
2024-04-23 16:27:41 +0800

    add bottom_tabbar navbar

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,21 +1,81 @@
-// index.ts
-// 获取应用实例
-const app = getApp<IAppOption>()
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+// pages/index/index.ts
+Page({
 
-Component({
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World111',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
-    },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    value: 'home',
+    ariaLabel: '首页',
+    list: [
+      { value: 'home', icon: 'home', ariaLabel: '首页' },
+      { value: 'medicine', icon: 'app', ariaLabel: '药品' },
+      { value: 'mine', icon: 'user', ariaLabel: '我的' },
+    ],
   },
-  methods: {
-    // 事件处理函数
-  
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
   },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  onChange(e: any) {
+    const value = e.detail.value;
+    const ariaLabel = this.data.list.find((item: any) => item.value === value)?.ariaLabel;
+    this.setData({
+      value,
+      ariaLabel,
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
 })

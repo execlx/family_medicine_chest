ff5fc0ff39a562998b6b478e4231e6dd91989b4e
codermy <gsmxsky@gmail.com>
2024-07-16 19:31:36 +0800

    iconify

--- src/components/mine.vue
+++ src/components/mine.vue
@@ -2,6 +2,7 @@
   <div class="p-4">
     <h1 class="text-2xl font-bold mb-4">Mine Page</h1>
     <!-- Mine page content here -->
+    <span class="i-mdi-abacus" style="color: #81c256;"></span>
   </div>
 </template>
 
